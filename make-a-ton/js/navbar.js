a44fb5332ca37db6da3ca3eb6d361f372f032219
sahil-athrij <sahilathrij@gmail.com>
2018-11-20 23:32:19 +0530

    Done

--- make-a-ton/js/navbar.js
+++ make-a-ton/js/navbar.js
@@ -3,17 +3,18 @@
 previd = "navhome";
 prevpage = "#intro";
 function makeactive(id,page) {
-    var NAME = document.getElementById(id);
-    NAME.className="active";
     var NAME = document.getElementById(previd);
     NAME.className=" ";
+    var NAME = document.getElementById(id);
+    NAME.className="active";
+
     previd = id;
     $(prevpage).css('z-index', '-2');
     $(prevpage).animate({
         opacity:0
 
     },500,function () {
-        $(prevpage).css('z-index', '2');
+        $(page).css('z-index', '2');
         $(page).animate({
             opacity:1
 
--- make-a-ton/js/page.js
+++ make-a-ton/js/page.js
@@ -1,4 +1,37 @@
+
+time = Date.now();
+top1 = $(window).scrollTop()
 $(document).ready(function(){
             $("#Introcaption").slideDown(500);
 
+
+
+});
+
+index = 1
+
+dic = {1: "makeactive('navhome', '#intro');",
+    2:"makeactive('navprize', '#prizes');",
+    3:"makeactive('navpartner', '#partners');",
+    4:"makeactive('navcont', '#contactUs');"}
+
+$(window).scroll(function(){
+    if ((time+1000-Date.now())<0) {
+
+        if(top1 - $(window).scrollTop() < 0){
+            top1 = $(window).scrollTop();
+            if(index < 4)
+            index += 1;
+            eval(dic[index]);
+            console.log("increment")
+        }
+        else if(top1 - $(window).scrollTop() > 0){
+            top1 = $(window).scrollTop();
+            if(index > 0)
+            index -= 1;
+            eval(dic[index]);
+            console.log("decrement")
+        }
+        time = Date.now()
+    }
 });
